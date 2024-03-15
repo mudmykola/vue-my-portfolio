e9c1ee845bf2cc20add14c6eb7643bec145bb945
mudmykola <mykola.mud@gmail.com>
2024-03-16 01:17:20 +0200

    Added social networks

--- src/components/about/AboutContactComponent.vue
+++ src/components/about/AboutContactComponent.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref } from 'vue';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const contactInfo = [
   { label: 'Age', value: calculateAge(new Date('1988-04-18')), class: 'about-contact__age--info' },
   { label: 'Residence', value: 'Ukraine', class: 'about-contact__residence--info' },
@@ -25,6 +27,18 @@ const getHref = (label, value) => {
   }
   return '#';
 };
+
+const socialLinks = ref({
+  github: 'https://github.com/mudmykola',
+  instagram: 'https://www.instagram.com/mud.software.engineer/',
+  linkedin: 'https://www.linkedin.com/in/mud-developer/'
+});
+
+const socialIcons = {
+  github: faGithub,
+  instagram: faInstagram,
+  linkedin: faLinkedin,
+};
 </script>
 
 <template>
@@ -40,6 +54,15 @@ const getHref = (label, value) => {
             <span v-else>
               <a :href="getHref(info.label, info.value)" target="_blank" class="cursor-pointer">{{ info.value }}</a>
             </span>
+          </li>
+        </ul>
+      </div>
+      <div class="about-contact__social mt-4">
+        <ul class="flex items-center gap-3">
+          <li v-for="(link, icon) in socialLinks" :key="icon">
+            <a :href="link" target="_blank">
+              <font-awesome-icon class="text-2xl hover:text-[#ca0de1]  transition-[.5s] hover:transition-all" :icon="socialIcons[icon]" />
+            </a>
           </li>
         </ul>
       </div>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import './assets/style/global-style.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+library.add(faGithub, faInstagram, faLinkedin);
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +15,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router)
 
 app.mount('#app')
