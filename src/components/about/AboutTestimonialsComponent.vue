03358d22e4c880b38cc7bebd82921c379da6a679
mudmykola <mykola.mud@gmail.com>
2024-03-17 13:48:46 +0200

    Added a fan block

--- src/components/about/AboutTestimonialsComponent.vue
+++ src/components/about/AboutTestimonialsComponent.vue
@@ -1,22 +1,14 @@
 <script setup>
-import {Swiper, SwiperSlide} from 'swiper/vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/swiper-bundle.css';
-import {ref} from 'vue';
+import { ref } from 'vue';
 import { useTestimonialsStore } from '@/stores/testimonial.js';
 
 const store = useTestimonialsStore();
 
 const testimonialsTitle = ref('Testimonials');
 
-const swiperOptions = {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  navigation: false,
-};
+
 </script>
 
 <template>
@@ -25,7 +17,7 @@ const swiperOptions = {
       <h2>{{ testimonialsTitle }}</h2>
     </div>
     <div class="testimonial-inner mt-5">
-      <Swiper :options="swiperOptions">
+      <Swiper>
         <template v-for="(group, index) in store.groupedTestimonials" :key="index">
           <SwiperSlide>
             <div class="testimonial-group flex justify-center gap-5">
@@ -34,7 +26,7 @@ const swiperOptions = {
                 <div class="testimonial-author flex items-center gap-2 pt-2">
                   <div class="testimonial-author__image">
                     <img width="60" height="60" :src="testimonial.photo" :alt="testimonial.author"
-                                                              class="photo rounded">
+                         class="photo rounded">
                   </div>
                   <div class="testimonial-author__text">
                     <p class="author text-[18px] font-bold">{{ testimonial.author }}</p>
