12e8d40d624d89d33e9cfdfc1755cecebcd45c53
mudmykola <mykola.mud@gmail.com>
2024-03-17 19:36:22 +0200

    I also added a title to the resume component and stylized it.

--- src/components/resume/ResumeComponent.vue
+++ src/components/resume/ResumeComponent.vue
@@ -1,11 +1,26 @@
 <script setup>
+import {ref} from "vue";
 
+const resumeTitle = ref('Resume')
+const resumeSubTitleItem = ref('3 Years of Experience\n')
 </script>
 
 <template>
-<div class="resume">Resume</div>
+<div class="resume top-[11%] left-[12%]  container-centered  animate__animated animate__backInDown">
+  <div class="resume-box">
+    <div class="resume-title  flex flex-col  items-center">
+      <div class="resume-title__box flex flex-col items-end">
+        <h1 class="text-6xl  font-bold">{{ resumeTitle }}</h1>
+        <div class="resume-subtitle flex text-center justify-center w-fit">
+          <p class="text-[15px]">{{ resumeSubTitleItem }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <style scoped lang="scss">
-
+@import "animate.css";
+@import "style";
 </style>
