622a1f088d10fe3f82d13e2081c06751f6d1751f
mudmykola <mykola.mud@gmail.com>
2024-03-20 00:01:31 +0200

    Added blocks with technologies

---
+++ src/components/resume/ResumeSkillToolsComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from "vue"
+const props = defineProps( ({
+  skillToolsTitle: String,
+  itemsTools: Array
+}))
+</script>
+
+<template>
+  <div class="tools  mt-5">
+    <div class="tools-title global-title text-2xl font-bold">
+      <h2>{{ skillToolsTitle }}</h2>
+    </div>
+    <div class="tools-inner mt-5">
+      <ul class="flex flex-wrap gap-4">
+        <li v-for="tech in itemsTools" :key="tech">
+          <h3 class="text-default font-bold text-[14px">{{ tech.name }}</h3>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "style";
+@import "src/assets/style/global-style";
+</style>
