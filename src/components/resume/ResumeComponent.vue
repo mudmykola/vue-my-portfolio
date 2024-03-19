ba00f80cd233eb40cbcbdb07ff4fa9a3ac12967b
mudmykola <mykola.mud@gmail.com>
2024-03-19 08:12:38 +0200

    Added a description of work experience and stylized

--- src/components/resume/ResumeComponent.vue
+++ src/components/resume/ResumeComponent.vue
@@ -1,9 +1,11 @@
 <script setup>
 import {ref} from "vue";
 import ResumeEducationComponent from "@/components/resume/ResumeEducationComponent.vue";
+import ResumeExperienceComponent from "@/components/resume/ResumeExperienceComponent.vue";
 
 const resumeTitle = ref('Resume')
 const educationTitle = ref('Education')
+const experienceTitle = ref('Experience')
 
 const resumeSubTitleItem = ref('3 Years of Experience\n')
 
@@ -11,6 +13,12 @@ const educationItems = ref([
   { year: "2022", title: "Front-end developer courses", institution: "Online Academy Ciklum", description: "Thanks to this course, I will be ready to create interactive interfaces as well as build full-fledged websites, from layout to hosting." },
   { year: "2021", title: "Course Front-end development", institution: "Beetroot Academy", description: "I gained a deep understanding of basic technologies such as HTML, CSS, and JavaScript, which are the foundation of any web developer. " },
   { year: "2020", title: "Web developer", institution: "WAYUP Academy", description: "Through the practical assignments and projects in the course, I gained experience with real-world web projects, including working with Git and GitHub to manage code versions and collaborate with other developers.  " },
+]);
+
+const experienceItems = ref([
+  { year: "2023 - Up to now", title: "Front End Engineer Shopify E-Commerce | Vue | Nuxt", institution: "Ecom-X Company", description: "Development and implementation of user interfaces for e-commerce on the Shopify platform, using Vue.js and Nuxt.js technologies." },
+  { year: "2023", title: "Front End Engineer Vue Composition API", institution: "Dozor Product Company", description: "Code optimization, changing the logic to the Composition API, working with GraphQL. " },
+  { year: "2022 - 2023", title: "Development of a company website", institution: "Freelance project AWW Company", description: "Development and implementation of interface components such as menus, carousels, order forms, product filters, etc. " },
 ]);
 </script>
 
@@ -25,8 +33,9 @@ const educationItems = ref([
           </div>
         </div>
       </div>
-      <div class="resume-list">
+      <div class="resume-list flex items-baseline gap-5">
         <ResumeEducationComponent :educationTitle="educationTitle" :items="educationItems"/>
+        <ResumeExperienceComponent :experienceTitle="experienceTitle" :items="experienceItems"/>
       </div>
     </div>
   </div>
