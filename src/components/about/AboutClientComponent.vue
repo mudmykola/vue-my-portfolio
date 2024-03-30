d1a4d2883a036e163ab52ca826c920560162c767
mudmykola <mykola.mud@gmail.com>
2024-03-30 23:11:07 +0200

    update api client v.1.1

--- src/components/about/AboutClientComponent.vue
+++ src/components/about/AboutClientComponent.vue
@@ -1,32 +1,29 @@
 <script setup>
-import { ref } from 'vue'
-const props = defineProps( ({
-  title: String
-}))
+import {  onMounted } from 'vue'
+import { useClientImagesStore } from '@/stores/clientImage.js';
 
-const clientItem = ref([
-  {image: 'Frame 147.png'},
-  {image: 'Frame 148.png'},
-  {image: 'Frame 149.png'},
-  {image: 'Frame 150.png'},
-  {image: 'Frame 153.png'},
-  {image: 'Frame 152.png'}
-])
+const props = defineProps({
+  title: String
+});
+
+const { clientImages, fetchClientImages } = useClientImagesStore();
+
+onMounted(fetchClientImages);
 </script>
 
 <template>
-<div class="services clients mt-10">
-  <div class="services-title clients-title text-2xl font-bold">
-    <h2>{{ title }}</h2>
+  <div class="services clients mt-10">
+    <div class="services-title clients-title text-2xl font-bold">
+      <h2>{{ title }}</h2>
+    </div>
+    <div class="clients-logo mt-5">
+      <ul class="flex items-center justify-between">
+        <li v-for="client in clientImages" :key="client.id">
+          <img width="150" height="150" :src="client.image" :alt="client.alt" loading="lazy">
+        </li>
+      </ul>
+    </div>
   </div>
-  <div class="clients-logo mt-5">
-    <ul class="flex items-center justify-between">
-      <li v-for="client in clientItem" :key="client">
-        <img width="150" height="150" :src="`src/assets/images/${client.image}`" alt="clients-logo" loading="lazy">
-      </li>
-    </ul>
-  </div>
-</div>
 </template>
 
 <style scoped lang="scss">
