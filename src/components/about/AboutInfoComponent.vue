3d491bd8bbc6b55d8f9869efd6f9ba80767f33dd
Mykola Mud` <46628437+mudmykola@users.noreply.github.com>
2024-04-03 23:05:57 +0300

    Feature/vmp portfolio (#33)
    * Revert "Feature/vmp portfolio (#28)"
    
    This reverts commit 734855e4f20daaa9b0d350d2114fb40981bf8ad4.
    
    * update api services v.1.2
    
    update api services v.1.1
    
    update api client v.1.1
    
    * Revert "update api services v.1.2"
    
    This reverts commit 93bc74308a77987da508d949a057d822855e3d9f.
    
    * update api testimonials v.1.1
    
    * update api about-info v.1.4
    
    * update api services v.1.4
    
    * update api services v.1.5
    
    * update api services v.1.6

--- src/components/about/AboutInfoComponent.vue
+++ src/components/about/AboutInfoComponent.vue
@@ -16,10 +16,6 @@ const rotateImage = (event) => {
 const aboutInfoTitle = ref('I am Software Engineer @ Ecom-X');
 
 const aboutInfoDesc = ref([
-  { id: 1, text: "I specialize in creating high-quality and intuitive interfaces. I have experience working with clients in various industries, as well as experience in Agile/Scrum methodology." },
-  { id: 2, text: "I believe that my potential as a front-end developer lies in the fact that I can create web applications that are not only beautiful and functional, but also user-friendly. I'm always looking for new ways to improve the user experience, and I'm always up to date with the latest trends in web application development." },
-  { id: 3, text: "I am proficient in a wide range of technologies that allow me to create innovative web applications. I'm fluent in CSS, HTML, SCSS, Vue, Nuxt, Vuex, Pinia, TailwindCSS, Shopify CLI, Theme Kit, Shopify e-commerce, Axios, Node.js, JavaScript, API, Liquid. I'm also familiar with GraphQL, Git, Docker." }
-]);
 
 const fetchAvatar = async () => {
   try {
